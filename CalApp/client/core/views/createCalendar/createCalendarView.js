a0d9b943936429f1c85bcd3aae21bf18d4a958be
Tiffany <tiffanylarsson.stud@gmail.com>
2026-03-06 15:15:33 +0100

    Update createCalendarView.js

--- CalApp/client/core/views/createCalendar/createCalendarView.js
+++ CalApp/client/core/views/createCalendar/createCalendarView.js
@@ -1,5 +1,34 @@
 // Tiffny
 
+
+export class CreateCalendarView {
+    
+    constructor(root) {
+        this.root = root;
+    }
+    
+    render() {
+        // HTML
+    }
+    
+    addListeners() {
+        // Eventlisteners och senare pubsub
+    }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 
     Varje view bestämmer vilka events som är relevanta för viewns komponenter. Komponenterna kopplas ej till store själva. 
