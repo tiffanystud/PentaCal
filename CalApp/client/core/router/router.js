6f2d5de2ce45c19668efdc99494e83a08737e64c
Tiffany <tiffanylarsson.stud@gmail.com>
2026-03-11 19:29:07 +0100

    Update router.js

--- CalApp/client/core/router/router.js
+++ CalApp/client/core/router/router.js
@@ -1,65 +1,56 @@
-// export function handleRouter(pathName) {
-//     let segments = pathName.split("/").filter(Boolean);
-
-//     if (segments[0] === "client") {
-//         segments.splice(0, 1);
-//     }
-//     console.log(`ROUTER: ${segments}`);
-
-
-//     if (segments[0] === "home" && segments[1] === "myGroupView") {
-//         console.log("myGroupView");
-//     } else if (segments[0] === "home" && segments[1] === "myCalView") {
-//         console.log("myCalView");
-//     } else if (segments[0] === "home") {
-//         console.log("home page");
-//     } else {
-//         console.log("404: Page not found");
-//     }
-// }
-
 
 import { CreateNotificationsView } from "../views/notifications/notifications.js";
 import { createGroupLandingView } from "../views/groupLanding/groupLanding.js";
 import { HomeView } from "../views/home/homeView.js";
+import { store } from "../store/store.js";
 
+function resolveRoute(path) {
+    const cleanPath = path.split("?")[0];
 
-
-
-const routes = {
-    "/CalApp/index.html": () => {
-        console.log("/CalApp/index.html page");
-    },
-    "": () => {
-        const app = document.querySelector("#app")
-        const view = new HomeView();
-        app.replaceChildren(view);
-    },
-    "groupsView": () => {
-        console.log("/groupsView page");
-        myGroupView();
-    },
-    "createNewCalendar": () => {
-        const view = new CreateCalendarView(document.querySelector("#app"));
-        view.render();
-    },
-    "notifications": () => {
-        const view = new CreateNotificationsView(document.querySelector("#app"));
-        view.render();
-    },
-    "groupLandingView": () => {
-        createGroupLandingView();
+    // Dynamisk route: /events/event/3
+    if (cleanPath.startsWith("/events/event/")) {
+        const id = cleanPath.split("/").pop();
+        store.setState({
+            currentPage: "eventDetails",
+            params: { id }
+        });
+        store.notify("pageChanged");
+        return;
     }
-};
 
-export function UrlRouter() {
-    const pathName = window.location.pathname;
-    const path = pathName.split("/").pop();
-    if (routes[path]) {
-        routes[path]();
-    } else {
-        console.warn("No route found:", path);
+    switch (cleanPath) {
+        case "/":
+            store.setState({ currentPage: "home" });
+            break;
+
+        case "/about":
+            store.setState({ currentPage: "about" });
+            break;
+
+        case "/calendar":
+            store.setState({ currentPage: "calendar" });
+            break;
+
+        default:
+            store.setState({ currentPage: "notfound" });
+            break;
     }
+
+    store.notify("pageChanged");
 }
 
+export const Router = {
+    navigate(path) {
+        history.pushState({}, "", path);
+        resolveRoute(path);
+    },
+
+    init() {
+        resolveRoute(window.location.pathname);
+
+        window.addEventListener("popstate", () => {
+            resolveRoute(window.location.pathname);
+        });
+    }
+};
 
