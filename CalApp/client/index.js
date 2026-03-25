5252dd13ee32406d655b27509720f9864c01b685
Tiffany <tiffanylarsson.stud@gmail.com>
2026-03-25 17:31:18 +0100

    Update desicionPopup

--- CalApp/client/index.js
+++ CalApp/client/index.js
@@ -56,9 +56,6 @@ PubSub.subscribe("change:view", (data) => {
 // START
 newRouter.init();
 
-
-
-
 /* PubSub.subscribe("change:view", (data) => {
     console.log("INDEX.JS RECEIVED:", data.mainPath, data.subPath);
     
